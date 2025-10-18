f69a8fd3119238bfd82304f8dabf118f8ff41379
Dmitry <dima.salnikov.2014@inbox.ru>
2025-10-18 13:56:56 +0700

    cocg доделан

--- BlockSLAE/Solvers/COCGSolver.cs
+++ BlockSLAE/Solvers/COCGSolver.cs
@@ -11,14 +11,16 @@ public class COCGSolver : Method<COCGConfig>
     private ComplexDiagonalPreconditioner _preconditioner = null!;
     private ComplexEquation<BlockMatrix> _equation = null!;
 
+    private double _r0Norm;
+
     private ComplexVector _r;
     private ComplexVector _z;
     private ComplexVector _p;
-    
+
     private ComplexVector _rNext;
     private ComplexVector _zNext;
     private ComplexVector _pNext;
-    
+
     public COCGSolver(
         ComplexDiagonalPreconditionerFactory factory,
         ILogger<COCGSolver> logger,
@@ -43,28 +45,44 @@ public class COCGSolver : Method<COCGConfig>
         _r = _equation.RightSide - _equation.Matrix.MultiplyOn(_equation.Solution);
         _z = _preconditioner.MultiplyOn(_r);
         _p = _z.Clone();
+        
+        _r0Norm = _r.Norm;
     }
 
     private ComplexVector IterationProcess()
     {
         var solution = _equation.Solution;
-        
-        for (var i = 1; i < Config.MaxIterations && _r.Norm / _equation.RightSide.Norm > Config.Epsilon; i++)
+        var fNorm = _equation.RightSide.Norm;
+
+        for (var i = 1; i < Config.MaxIterations && _r.Norm / fNorm >= Config.Epsilon; i++)
         {
-            var a = _r.PseudoScalarProduct(_z) / _equation.Matrix.MultiplyOn(_p).PseudoScalarProduct(_p);
+            var matrixByP = _equation.Matrix.MultiplyOn(_p);
+
+            var a = _r.PseudoScalarProduct(_z) / matrixByP.PseudoScalarProduct(_p);
             solution += _p.MultiplyOn(a);
 
-            _rNext = _r - _equation.Matrix.MultiplyOn(_p).MultiplyOn(a);
+            _rNext = _r - matrixByP.MultiplyOn(a);
             _zNext = _preconditioner.MultiplyOn(_rNext);
 
             var b = _rNext.PseudoScalarProduct(_zNext) / _r.PseudoScalarProduct(_z);
             _pNext = _zNext + _p.MultiplyOn(b);
-            
-            _r =  _rNext.Clone();
-            _z =  _zNext.Clone();
-            _p =  _pNext.Clone();
+
+            _r = _rNext.Clone();
+            _z = _zNext.Clone();
+            _p = _pNext.Clone();
+
+            if (i % 200 == 0)
+            {
+                Logger.LogInformation("[{Iteration}] {relativeDiscrepancy:E15} / {Config.Discrepancy}", i, _r.Norm / fNorm, Config.Epsilon);
+                Console.WriteLine($"Iteration: {i} {_r.Norm / fNorm:E15} / {Config.Epsilon:E15}");
+            }
         }
+
+        var discrepancy = (_equation.RightSide - _equation.Matrix.MultiplyOn(solution)).Norm / _r0Norm;
         
+        Logger.LogInformation("Discrepancy: {discrepancy:E8}", discrepancy);
+        Console.WriteLine($"Discrepancy: {discrepancy:E8}");
+
         return solution;
     }
 }
--- BlockSLAE/Storages/ComplexVector.cs
+++ BlockSLAE/Storages/ComplexVector.cs
@@ -7,7 +7,9 @@ public class ComplexVector
     public double[] Values { get; }
     public int Length => Values.Length;
 
-    public double Norm => double.Sqrt(ScalarProduct(this, this).Real);
+    public double Norm
+        //=> double.Sqrt(ScalarProduct(this, this).Real);
+        => double.Sqrt(DomnikovScalarProduct(this, this));
 
     public ComplexVector(IEnumerable<double> values)
     {
@@ -27,6 +29,32 @@ public class ComplexVector
     public ComplexVector Clone()
     {
         return new ComplexVector(Values);
+    }
+
+    public ComplexVector MultiplyOn(Complex complexScalar)
+    {
+        var resultMemory = Clone();
+
+        for (var i = 0; i < Length / 2; i++)
+        {
+            var z = new Complex(Values[i * 2], Values[i * 2 + 1]);
+            var aByZ = complexScalar * z;
+
+            resultMemory.Values[i * 2] = aByZ.Real;
+            resultMemory.Values[i * 2 + 1] = aByZ.Imaginary;
+        }
+
+        return resultMemory;
+    }
+
+    public Complex ScalarProduct(ComplexVector outerVector)
+    {
+        return ScalarProduct(this, outerVector);
+    }
+
+    public Complex PseudoScalarProduct(ComplexVector outerVector)
+    {
+        return PseudoScalarProduct(this, outerVector);
     }
 
     public static ComplexVector operator +(ComplexVector vectorLeft, ComplexVector vectorRight)
@@ -63,32 +91,18 @@ public class ComplexVector
         return resultMemory;
     }
 
-    public ComplexVector MultiplyOn(Complex complexScalar)
+    private static ComplexVector Conjugate(ComplexVector vector)
     {
-        var resultMemory = Clone();
+        var values = vector.Values;
 
-        for (var i = 0; i < Length / 2; i++)
+        for (var i = 1; i < vector.Length; i += 2)
         {
-            var z = new Complex(Values[i * 2], Values[i * 2 + 1]);
-            var aByZ = complexScalar * z;
-
-            resultMemory.Values[i * 2] = aByZ.Real;
-            resultMemory.Values[i * 2 + 1] = aByZ.Imaginary;
+            values[i] = -vector.Values[i];
         }
-        
-        return resultMemory;
+
+        return new ComplexVector(values);
     }
 
-    public Complex ScalarProduct(ComplexVector outerVector)
-    {
-        return ScalarProduct(this, outerVector);
-    }
-
-    public Complex PseudoScalarProduct(ComplexVector outerVector)
-    {
-        return ScalarProduct(this, outerVector);
-    }
-    
     private static Complex ScalarProduct(ComplexVector a, ComplexVector b) // 2
     {
         if (a.Length % 2 != 0 || b.Length % 2 != 0)
@@ -112,12 +126,17 @@ public class ComplexVector
 
         return sum;
     }
-    
+
     private static Complex PseudoScalarProduct(ComplexVector a, ComplexVector b) // 3
+    {
+        return ScalarProduct(Conjugate(a), b);
+    }
+    
+    private static double DomnikovScalarProduct(ComplexVector a, ComplexVector b) // 4
     {
         if (a.Length % 2 != 0 || b.Length % 2 != 0)
         {
-            throw new ArgumentException("Vectors must have an even length");
+            throw new ArgumentException("Vector and result must have an even length");
         }
 
         if (a.Length != b.Length)
@@ -125,13 +144,10 @@ public class ComplexVector
             throw new ArgumentException("Vector and result must have same length");
         }
 
-        Complex sum = 0;
-        for (var i = 0; i < a.Length / 2; ++i)
+        var sum = 0d;
+        for (var i = 0; i < a.Length / 2; i++)
         {
-            var aComplex = new Complex(a.Values[2 * i], a.Values[2 * i + 1]);
-            var bComplex = new Complex(b.Values[2 * i], b.Values[2 * i + 1]);
-
-            sum += aComplex * bComplex;
+            sum += a.Values[2 * i] * b.Values[2 * i] + a.Values[2 * i + 1] * b.Values[2 * i + 1];
         }
 
         return sum;
